13ba61287732dd2b263dc2b0f8c70d654f72e5f6
Laszlo Teveli <tevelee@gmail.com>
2024-10-03 01:00:00 +0200

    Minimum deployment target specified
    'some' return types are only available in iOS 13.0.0 or newer

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -4,6 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "Graphs",
+    platforms: [
+        .iOS(.v13),
+        .macCatalyst(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v6),
+        .tvOS(.v13),
+        .visionOS(.v1)
+    ],
     products: [
         .library(
             name: "Graphs",
